f630dd644407ec08a9ab82ccaad23d7b3a1717ac
Sidney Andrews <sidney@seesharprun.net>
2020-08-14 11:20:26 -0400

    Removed package lock (Don't like it)

--- components/navigation.js
+++ components/navigation.js
@@ -1,11 +1,7 @@
-import styles from "./navigation.module.css"
-
 const Navigation = () =>
     <nav className="navbar navbar-dark bg-dark">
         <span className="navbar-brand mb-0 h1">
-            <span className={styles.yellow}>
-                Internet
-            </span>
+            Internet
         </span>
     </nav>;
 
